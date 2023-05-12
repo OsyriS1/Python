4ea03ab32222df8b0054cfbc9cd5195d31a6e1da
Sergey <Syris136@gmail.com>
2023-05-12 11:39:57 +0300

    complete

--- lesson_1.py
+++ lesson_1.py
@@ -86,18 +86,42 @@
 
 #Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
 
-def power(x, n):
-    if n == 0:
-        return 1
-    if n < 0:
-        return 1 / power(x, -n)
-    if n % 2 == 0:
-        return power(x, n // 2) * power(x, n // 2)
-    else:
-        return power(x, n - 1) * x
+# def power(x, n):
+#     if n == 0:
+#         return 1
+#     if n < 0:
+#         return 1 / power(x, -n)
+#     if n % 2 == 0:
+#         return power(x, n // 2) * power(x, n // 2)
+#     else:
+#         return power(x, n - 1) * x
 
-x = int(input())
-n = int(input())
-print(power(x, n))
+# x = int(input())
+# n = int(input())
+# print(power(x, n))
+
+#Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел.
+#Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
+
+def sum (a, b):
+        a += 1
+        b -= 1
+        if b > 0:
+           return sum (a, b)
+        else: 
+           return a 
+    
+a = int (input("Введите А: "))
+b = int (input("Введите Б: "))
+print (sum (a, b))
 
 
+#Задача 30:  Заполните массив элементами арифметической прогрессии.
+#  Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. 
+# Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+#Каждое число вводится с новой строки
+
+n_one = int (input())
+diff = int (input())
+amount = int (input())
+array 
